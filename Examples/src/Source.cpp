e04465b66ecb705c182a7fe16294799e41d78b7a
vakho10 <vakho10@gmail.com>
2018-10-27 11:31:07 +0400

    Extracted example from root project and refactored the names.

---
+++ Examples/src/Source.cpp
@@ -0,0 +1,18 @@
+#include "..\..\SuperLoggingProject\src\Logger.h"
+#include "Cat.h"
+
+int main() {
+
+	Logger* logger = new Logger();
+	logger->setFilter(LoggingLevel::TRACE);
+	logger->addFileLog("resources\\log.txt");
+
+	// Cat logic
+	Cat* cat = new Cat("Piso");
+	std::cout << cat->sound() << std::endl;
+
+	delete logger;
+	logger = NULL;
+
+	system("PAUSE");
+}
