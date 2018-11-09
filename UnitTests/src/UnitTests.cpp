a3234bec338b5fb745b52bb405259526146d937f
Vakhtang Laluashvili <v.laluashvili@sda.gov.ge>
2018-11-09 14:13:22 +0400

    Changed unit test.

--- UnitTests/src/UnitTests.cpp
+++ UnitTests/src/UnitTests.cpp
@@ -9,15 +9,19 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTests
-{		
+{
 	TEST_CLASS(LoggerTest)
 	{
 	public:
+
+		/*
+			1) Set level to INFO
+			2) Print each level of logs into the string stream
+			3) Read string stream and check if new lines are exactly four
+		*/
 		TEST_METHOD(FilterTests)
 		{
 			std::ostringstream* ost = new std::ostringstream();
-
-			// TODO check result
 			Sangu::Logger logger = Sangu::Logger(Sangu::LoggingLevel::INFO, ost);
 			logger.trace("x");
 			logger.debug("x");
@@ -27,7 +31,9 @@ namespace UnitTests
 			logger.fatal("x");
 			std::string str = ost->str();
 
-			Assert::IsTrue(false); // Fail
+			// Check number of occurrences
+			int numberOfLines = std::count(str.begin(), str.end(), '\n');
+			Assert::AreEqual(4, numberOfLines);
 		}
 
 	};
